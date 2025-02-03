6eaf1f6eb399dc21cc97c924bc5f71a09f9ac689
test-user-cc <manojvaddi497@gmail.com>
2025-02-03 16:41:08 -0500

    added mysql

---
+++ mysql.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGFILE_DIR="/var/log/expense_shell"
+LOGFILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%m-%d-%Y-%H-%M-%S)
+LOGFILE_NAME="$LOGFILE_DIR/$LOGFILE-$TIMESTAMP.log"
+
+#check if the user is root
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Error: user should have root privileges"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is... $R Failed $N"
+        exit 1
+    else
+        echo "$2 is... $G Successful $N"
+    fi
+}
+
+dnf list installed mysql-server &>>$LOGFILE_NAME
+if [ $? -ne 0 ]
+then
+    dnf install mysql-server -y &>>$LOGFILE_NAME
+    VALIDATE $? "MYSQL Server Installation is..."
+else
+    echo "MYSQL Server Installtion already exists"
+fi
+systemctl enable mysqld
+VALIDATE $? "MYSQL Server enabled.."
+systemctl start mysqld
+VALIDATE $? "MYSQL Server Started.."
+mysql_secure_installation --set-root-pass ExpenseApp@1 -e show databases;
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Root Password is set.."
+else
+    echo "ROOT Password is already set"
+fi
+
+
